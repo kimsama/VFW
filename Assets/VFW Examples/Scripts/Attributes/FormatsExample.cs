4715b7573aa3dde0d8c9b3f8b22a30bae7a5eab0
kimsama <hwpeterkim@gmail.com>
2015-03-31 20:39:26 +0900

    initial commit.

---
+++ Assets/VFW Examples/Scripts/Attributes/FormatsExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vexe.Runtime.Types.Examples
+{
+	[BasicView]
+	public class FormatsExample : BetterBehaviour
+	{
+		[FormatMember("$name :: $type")]
+		public GameObject[] GameObjects { get; set; }
+
+		[FormatMember("$name"), FormatPair("[Key: $key, Value: $value]")]
+		public Dictionary<string, Transform> Transforms { get; set; }
+	}
+}
---
+++ Assets/VFW Examples/Scripts/Attributes/ShowMethodExample.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Vexe.Runtime.Types.Examples
+{
+	/// <summary>
+	/// A small demo for exposing methods in the inspector
+	/// Notes: The arguments you set to the method in the editor won't survive assembly reloads
+	/// </summary>
+	[BasicView]
+	public class ShowMethodExample : BetterBehaviour
+	{
+		[Show]
+		public void Method0()
+		{
+			Log("void - no params");
+		}
+
+		[Show]
+		public void Method1(string arg0)
+		{
+			name = arg0;
+		}
+
+		[Show]
+		public int Method2(Transform arg0, ITest arg1)
+		{
+			Log("returning " + arg0.name + " children count: " + arg0.childCount);
+			Log(arg1.Vector);
+			return arg0.childCount;
+		}
+
+		string[] ItemNames
+		{
+			get { return new string[]
+			{
+				"Shotgun",
+				"SomeAmmo",
+				"MansionKey",
+				"etc"
+			}; }
+		}
+
+		[Show] void Method3(
+			[Popup("ItemNames")] string item,
+			[Tags, FilterTags] string tag,
+			[SelectEnum] KeyCode key)
+		{
+			Log(item);
+			Log(tag);
+			Log(key);
+		}
+	}
+}
